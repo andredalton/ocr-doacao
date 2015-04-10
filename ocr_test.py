db728948e9cbfbed53ac7f5c9f07a3041f72861f
OCR <ocr@parrudo.defaultdomain>
2015-04-10 15:45:26 +0000

    Testes iniciais finalizados.

--- ocr_test.py
+++ ocr_test.py
@@ -1,39 +1,37 @@
 import os, random, unittest, json
 import sys, re, requests, time
 
-def get_contents(path):
-    #content = ""
-    with open(path, 'r') as content_file:
-        content = content_file.read()
-    return content
+def get_contents(imagename):
+    """
+    A funcao pega o conteudo de um arquivo de gabarito
+    e compara com a resposta do webservice.
+    """
+    (path, ext) = os.path.splitext(imagename)
+    txtname = path + ".txt"
+    with open(txtname, 'r') as content_file:
+        txtcontent = content_file.read()
+    r = requests.post('http://localhost:5000', files={'file': open(imagename, 'rb')})
+    imagecontent = r.json()["text"]
+    return (txtcontent, imagecontent)
 
 class TestOCR(unittest.TestCase):
-    def setUp(self):
-        """ Iniciando."""
-        pass
-        
     def test_blank_image(self):
         """ Verifica se as duas instancias sao distintas."""
-        # print get_contents("test/bigblank.txt")
-        self.assertNotEqual( 4, 2)
-
+        (txt, image) = get_contents('test/bigblank.jpg')
+        self.assertEqual( "".join(txt.split()), "".join(image.split()))
+    
+    def test_nf_text(self):
+        (txt, image) = get_contents('test/nf.jpg')
+        self.assertEqual( "".join(txt.split()), "".join(image.split()))
+ 
     def test_photo_image(self):
         """ Verifica se as duas instancias sao distintas."""
-        # r = requests.post('http://localhost:5000', files={'file': open('test/xp.jpg', 'rb')})
-        # print "\n\n", r.text, "\n\n"
-        self.assertNotEqual( 4, 2)
+        (txt, image) = get_contents('test/xp.jpg')
+        self.assertEqual( "".join(txt.split()), "".join(image.split()))
     
     def test_quality_text(self):
-        # print get_contents("test/abc.txt")
-        r = requests.post('http://localhost:5000', files={'file': open('test/abc.jpg', 'rb')})
-        print "\n\n", json.loads(r.text)["text"], "\n\n"
-        print "\n\n", r.json()["text"], "\n\n"
-        self.assertNotEqual( 4, 2)
-
-    def test_nf_text(self):
-        # r = requests.post('http://localhost:5000', files={'file': open('test/nf.jpg', 'rb')})
-        # print "\n\n", r.text, "\n\n"
-        self.assertNotEqual( 4, 2)
+        (txt, image) = get_contents('test/abc.jpg')
+        self.assertEqual( "".join(txt.split()), "".join(image.split()))
 
 if __name__ == '__main__':
     unittest.main()
