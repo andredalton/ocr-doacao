de5031f06f568ff8df95005ca78f51723cd3835f
André Meneghelli Vale <andredalton@gmail.com>
2015-05-02 04:44:37 -0300

    Separando javascript para upload de imagem de ong.html. [André, Marcello]

--- ocr/js/preload.js
+++ ocr/js/preload.js
@@ -1,7 +1,6 @@
 function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
-
         reader.onload = function (e) {
             $('#preview_photo').attr('src', e.target.result);
             $photo = $('#preview_photo');
@@ -11,5 +10,14 @@ function readURL(input) {
             }
         }
         reader.readAsDataURL(input.files[0]);
+        }
     }
-}
+    $("#file1").change(function(){
+        readURL(this);
+    });
+$("#imgInp").change(function(){
+    readURL(this);
+});
+$("#upfile1").click(function () {
+    $("#file1").trigger('click');
+});
